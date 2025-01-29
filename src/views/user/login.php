37d1c2e2d3bbb681d1d827e6806407489ec0d2e0
Rubens Lira <rubenslira1156@gmail.com>
2025-01-28 22:54:10 -0300

    CRUD WEB

---
+++ src/views/user/login.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <h2>Login</h2>
+    <form action="index.php?action=login" method="post">
+        E-mail
+        <label for="email">
+            <input 
+                type="email"
+                id="email"
+                name="email"
+                class="input"
+                required
+            />
+        </label>
+        <label for="senha">
+            Senha
+            <input 
+                type="password"
+                id="senha"
+                name="senha"
+                class="input"
+                required
+            />
+        </label>
+        <input type="submit" class="enviar" value="Enviar">
+    </form>
+</div>
